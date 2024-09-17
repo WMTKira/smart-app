3b108df5b1596f1695258e3ec48132efbe02e859
wmtumanday <wiljohnyumanday@gmail.com>
2024-09-18 02:52:23 +0800

    debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { UserListComponent } from "./user-list/user-list.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, UserListComponent, CommonModule],
+  imports: [RouterModule, UserListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
