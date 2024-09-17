09ad1379536fdaebb2537ccb48fd5f2e3353eea3
wmtumanday <wiljohnyumanday@gmail.com>
2024-09-18 00:48:29 +0800

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from "./user-list/user-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
