1686823972b8df4dd20229be176afee86e6e38f8
wmtumanday <wiljohnyumanday@gmail.com>
2024-09-18 01:03:20 +0800

    debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from "./user-list/user-list.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent],
+  imports: [RouterOutlet, UserListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'smart-app';
+  title = 'Smart Parking';
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { User } from '../class/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
 })
 export class UserListComponent {
+
+  users!: User[];
 
 }
